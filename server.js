14a7493a439d710c1b387aacc40d3b02f936657a
Glitch (excersize) <none>
2019-11-25 18:19:48 +0000

    💰🐉 Updated with Glitch

--- server.js
+++ server.js
@@ -35,18 +35,124 @@ app.post('/api/exercise/new-user', (req, res) => {
   let _id = shortid.generate()
   
   MongoClient.connect(dbUrl, (err, db) => {      
-      // check if username exist
-      
-      // add url to latest
-      db.collection('users').insertOne({_id, username})
-      .then(() => db.close())
+    // Check username exists
+     db.collection('users').find({username}).toArray( (err, results) => {       
+       if (results.length != 0) {
+         db.close()
+         res.send('user already exists with that name')
+       } else {
+         // create the user
+         db.collection('users').insertOne({_id, username})
+        .then(() => db.close())
+         
+        res.json({_id, username})
+       }
+     })
   })
-  
-  res.json({_id, username})
 })
 
 
 
+// Get users
+function getUsers(db, callback) {
+  db.collection('users').find().toArray((err, results) => callback(results))
+}
+
+app.get('/api/exercise/users', (req, res) => {
+  MongoClient.connect(dbUrl, function(err, db) {
+    getUsers(db, results => {
+      db.close()
+      res.json(results)
+    })
+  })
+})
+
+
+
+// Add excersize
+app.post('/api/exercise/add', (req, res) => {
+  let _id = req.body.userId
+  let description = req.body.description
+  let duration = req.body.duration
+  let date = req.body.date
+  
+  // if no date set then date is today
+  if (date == '') date = new Date().toISOString().slice(0, 10)
+  
+  MongoClient.connect(dbUrl, function(err, db) {
+    db.collection('users').update({_id}, { $push: { log: {description,duration,date} } })
+    .then(() => db.close())
+  })
+  
+  res.json({_id, description, duration, date})
+})
+
+
+
+// Get user log
+app.get('/api/exercise/log', (req, res) => {
+  let _id = req.query.userId
+  let from = req.query.from
+  let to = req.query.to
+  let limit = req.query.limit
+  
+  MongoClient.connect(dbUrl, (err, db) => {
+    
+    db.collection('users').findOne({_id}, (err, user) => {
+      let username = user.username
+      let log = user.log
+      let count = log.length
+      
+      db.close()
+      
+      // no parameters set
+      if (!from && !to && !limit) {
+        res.json({_id, username, count, log})
+        return
+      }
+      
+      // limit and no date
+      if (limit && !from && !to) {
+        let newLog = [] 
+        let endLimit = Number(limit)
+        for (let i = 0; i < endLimit; i++)
+          newLog.push(log[i])
+        
+        let newCount = newLog.length
+        res.json({_id, username, newCount, newLog})
+        return
+      }
+      
+      // limited date  
+      let newLog = []
+      
+      if (from || to) {
+        let fromDate = from ? Date.parse(from) : 0
+        let toDate = to ? Date.parse(to) : Number.MAX_VALUE
+        
+        for (let entry of log)
+          if (Date.parse(entry.date) > fromDate && Date.parse(entry.date) < toDate) {
+            newLog.push(entry)
+          }
+      }
+      
+      // limited log after date
+      if (limit) {
+        let limitedLog = []
+        let endLimit = Number(limit)
+        for (let i = 0; i < endLimit; i++)
+          limitedLog.push(newLog[i])        
+        newLog = limitedLog
+      }
+      
+      let newCount = newLog.length
+      res.json({_id, username, newCount, newLog})
+      return
+      
+    })
+  
+  })
+})
 
 
 
